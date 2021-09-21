de80ee3cca91252a55d5ecddd0efbdb89ee9b02e
kuni23 <84456950+kuni23@users.noreply.github.com>
2021-09-21 16:46:46 +0200

    Add files via upload

---
+++ Lab2.3/blobs.cpp
@@ -0,0 +1,351 @@
+/* Applied Video Analysis of Sequences (AVSA)
+ *
+ *	LAB2: Blob detection & classification
+ *	Lab2.0: Sample Opencv project
+ *
+ *
+ * Authors: José M. Martínez (josem.martinez@uam.es), Paula Moral (paula.moral@uam.es), Juan C. San Miguel (juancarlos.sanmiguel@uam.es)
+ */
+
+#include "blobs.hpp"
+
+using namespace std;
+
+/**
+ *	Draws blobs with different rectangles on the image 'frame'. All the input arguments must be
+ *  initialized when using this function.
+ *
+ * \param frame Input image
+ * \param pBlobList List to store the blobs found<<<<
+ * \param labelled - true write label and color bb, false does not wirite label nor color bb
+
+ * \return Image containing the draw blobs. If no blobs have to be painted
+ *  or arguments are wrong, the function returns a copy of the original "frame".
+ *
+ */
+ Mat paintBlobImage(cv::Mat frame, std::vector<cvBlob> bloblist, bool labelled)
+{
+	cv::Mat blobImage;
+	//check input conditions and return original if any is not satisfied
+	//...
+	frame.copyTo(blobImage);
+
+	//required variables to paint
+	//...
+
+	//paint each blob of the list
+	for(int i = 0; i < bloblist.size(); i++)
+	{
+		cvBlob blob = bloblist[i]; //get ith blob
+		//...
+		Scalar color;
+		std::string label="";
+
+		switch(blob.label){
+		case PERSON:
+
+			color = Scalar(255,0,0);
+			label="PERSON";
+			break;
+		case CAR:
+					color = Scalar(0,255,0);
+					label="CAR";
+					break;
+		case OBJECT:
+					color = Scalar(0,0,255);
+					label="OBJECT";
+					break;
+		default:
+			color = Scalar(255, 255, 255);
+			label="UNKOWN";
+		}
+
+		Point p1 = Point(blob.x, blob.y);
+		Point p2 = Point(blob.x+blob.w, blob.y+blob.h);
+
+		rectangle(blobImage, p1, p2, color, 1, 8, 0);
+		if (labelled)
+			{
+			rectangle(blobImage, p1, p2, color, 1, 8, 0);
+			putText(blobImage, label, p1, FONT_HERSHEY_SIMPLEX, 0.5, color);
+			}
+			else
+				rectangle(blobImage, p1, p2, Scalar(255, 255, 255), 1, 8, 0);
+	}
+
+	//destroy all resources (if required)
+	//...
+
+	//return the image to show
+	return blobImage;
+}
+
+
+/**
+ *	Blob extraction from 1-channel image (binary). The extraction is performed based
+ *	on the analysis of the connected components. All the input arguments must be 
+ *  initialized when using this function.
+ *
+ * \param fgmask Foreground/Background segmentation mask (1-channel binary image) 
+ * \param bloblist List with found blobs
+ *
+ * \return Operation code (negative if not succesfull operation) 
+ */
+int extractBlobs(cv::Mat fgmask, std::vector<cvBlob> &bloblist, int connectivity)
+{	
+	//check input conditions and return -1 if any is not satisfied
+	//...		
+
+	if((connectivity!=8) & (connectivity!=4))
+	{
+		cout<<"Bad connectivity input!!"<<endl;
+		return -1;
+	}
+
+	//required variables for connected component analysis 
+	//...
+	Mat aux; // image to be updated each time a blob is detected (blob cleared)
+	fgmask.copyTo(aux);
+	//clear blob list (to fill with this function)
+	bloblist.clear();
+			
+	//Connected component analysis
+
+	//Applying the floodfill algortihmm
+	 for (int i=0; i<aux.rows;i++)
+			 for(int j=0; j<aux.cols;j++)
+			 {
+				 if(aux.at<uchar>(i, j)==255)
+				 {
+					 Rect r;
+					 //23 is a random number, it must be not 255!
+					 floodFill(aux, Point(j, i), 23, &r, 0, 0, connectivity);
+					 cvBlob blob = initBlob(1, r.x, r.y, r.width, r.height);
+					 bloblist.push_back(blob);
+
+				 }
+
+			 }
+
+
+
+//	std::cout << bkg << " " << fg << " " << sh <<" " << fill << " " << unknown << " "<< bkg+fg+sh+unknown  << " " << fgmask.rows*fgmask.cols << std::endl;
+//	std::cout << blob_id << " " << small_blobs << std::endl;
+
+	//destroy all resources
+	//...
+
+	//return OK code
+	return 1;
+}
+
+
+int removeSmallBlobs(std::vector<cvBlob> bloblist_in, std::vector<cvBlob> &bloblist_out, int min_width, int min_height)
+{
+	//check input conditions and return -1 if any is not satisfied
+
+	//required variables
+	//...
+
+	//clear blob list (to fill with this function)
+	bloblist_out.clear();
+
+
+	for(int i = 0; i < bloblist_in.size(); i++)
+	{
+		cvBlob blob_in = bloblist_in[i]; //get ith blob
+
+		// ...
+
+		//Checking the min conditions
+		if((blob_in.w>min_width) | (blob_in.h>min_height))
+		{
+			bloblist_out.push_back(blob_in); // void implementation (does not remove)
+		}
+
+	}
+	//destroy all resources
+	//...
+	
+	//return OK code
+	return 1;
+}
+
+
+
+ /**
+  *	Blob classification between the available classes in 'Blob.hpp' (see CLASS typedef). All the input arguments must be
+  *  initialized when using this function.
+  *
+  * \param frame Input image
+  * \param fgmask Foreground/Background segmentation mask (1-channel binary image)
+  * \param bloblist List with found blobs
+  *
+  * \return Operation code (negative if not succesfull operation)
+  */
+
+ // ASPECT RATIO MODELS
+#define MEAN_PERSON 0.3950
+#define STD_PERSON 0.1887
+
+#define MEAN_CAR 1.4736
+#define STD_CAR 0.2329
+
+#define MEAN_OBJECT 1.2111
+#define STD_OBJECT 0.4470
+
+// end ASPECT RATIO MODELS
+
+// distances
+float ED(float val1, float val2)
+{
+	return sqrt(pow(val1-val2,2));
+}
+
+float WED(float val1, float val2, float std)
+{
+	return sqrt(pow(val1-val2,2)/pow(std,2));
+}
+//end distances
+ int classifyBlobs(std::vector<cvBlob> &bloblist)
+ {
+ 	//check input conditions and return -1 if any is not satisfied
+ 	//...
+
+ 	//required variables for classification
+ 	//...
+
+	vector<bool> result;
+	vector<double> diff;
+
+
+ 	//classify each blob of the list
+ 	for(int i = 0; i < bloblist.size(); i++)
+ 	{
+ 		cvBlob blob = bloblist[i]; //get i-th blob
+
+ 		double ratio= (double) blob.w/blob.h;
+
+ 		result.clear();
+ 		diff.clear();
+
+
+ 		//Variables in each cycle
+ 		bool person=abs(ratio-MEAN_PERSON)<STD_PERSON;
+ 		bool car = abs(ratio-MEAN_CAR)<STD_CAR;
+ 		bool object = abs(ratio-MEAN_OBJECT)<STD_OBJECT;
+
+ 		if(person) {blob.label=PERSON;}
+ 		//Problem can happen if car and object are also detected. So the one is closer to the ratio, will be the classified label.
+ 		if(car & object)
+ 		{
+ 			if(abs(ratio-MEAN_CAR)<abs(ratio-MEAN_OBJECT))
+ 			{
+ 				blob.label=CAR;
+ 			}
+ 			else
+ 			{
+ 				blob.label=OBJECT;
+ 			}
+ 		}
+ 		else
+ 		{
+ 	 		if(car) blob.label=CAR;
+ 	 		if(object) blob.label=OBJECT;
+ 		}
+
+
+ 		//...
+ 		// void implementation (does not change label -at creation UNKNOWN-)
+
+ 		bloblist[i]=blob;
+ 	}
+
+ 	//destroy all resources
+ 	//...
+
+ 	//return OK code
+ 	return 1;
+ }
+
+//stationary blob extraction function
+ /**
+  *	Stationary FG detection
+  *
+  * \param fgmask Foreground/Background segmentation mask (1-channel binary image)
+  * \param fgmask_history Foreground history counter image (1-channel integer image)
+  * \param sfgmask Foreground/Background segmentation mask (1-channel binary image)
+  *
+  * \return Operation code (negative if not succesfull operation)
+  *
+  *
+  * Based on: Stationary foreground detection for video-surveillance based on foreground and motion history images, D.Ortego, J.C.SanMiguel, AVSS2013
+  *
+  */
+
+#define FPS 25 //check in video - not really critical
+#define SECS_STATIONARY 3 // to set
+#define I_COST 0 // to set // increment cost for stationarity detection
+#define D_COST 0 // to set // decrement cost for stationarity detection
+#define STAT_TH 0.0 // to set
+
+ int extractStationaryFG (Mat fgmask, Mat &fgmask_history, Mat &sfgmask)
+ {
+
+	 int numframes4static=(int)(FPS*SECS_STATIONARY);
+
+	 //Background substraction is already performed  (we have the mask)
+
+	 //FGmask
+	 //		255 --> foreground
+	 //		127 --> shadow
+	 //		0 	--> background
+
+	 //measure FG temporal variation to get FG history img
+ 	 // update fgmask_counter --> increases for FG, decreases for BG
+
+	 float Wpos=1;
+	 float Wneg=4;
+	 float th = 0.4;
+
+	 for (int i=0; i<fgmask.rows;i++){
+		 for(int j=0; j<fgmask.cols;j++){
+			 if (fgmask.at<uchar>(i,j)==255){
+				 fgmask_history.at<float>(i,j) = fgmask_history.at<float>(i,j)+Wpos; // FHI (t) = FHI (t−1) + w * FG (t)
+			 }else{
+				 if(fgmask_history.at<float>(i,j)-Wneg < 0){
+					 fgmask_history.at<float>(i,j) = 0;
+				 }else{
+					 fgmask_history.at<float>(i,j) = fgmask_history.at<float>(i,j)-Wneg; // FHI (t) = FHI (t−1) + w * FG (t)
+				 }
+			 }
+		 }
+	 }
+
+	 // AT FGMASK_HISTORY WE HAVE A FOREGROUND SCORE, WHICH INCREASES WHEN THE PIXEL IS FG AND DECREASES WHEN IT IS BKG
+
+
+	 // "combination" and normalization
+	 //fgmask_history=min(255,(fgmask_history/numframes4static));
+
+	 cv::Mat fgmask_history_normalized=min(1,(fgmask_history/numframes4static));
+
+	 //update sfgmask
+	 for (int i=0; i<fgmask.rows;i++){
+		  for(int j=0; j<fgmask.cols;j++){
+			  if(fgmask_history_normalized.at<float>(i,j)>=th){
+			  	  sfgmask.at<uchar>(i,j)=255;
+			  }else{
+				  sfgmask.at<uchar>(i,j)=0;
+			  }
+		  }
+	 }
+
+
+	 //imshow("sfgmask",sfgmask);
+	 //imshow("norm FHI",fgmask_history_normalized);
+
+ return 1;
+ }
+
+
